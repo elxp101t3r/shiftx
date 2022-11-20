24b6b6c98923ab5d91884abbf4869f301b531402
elxp101t3r <eliotx10101@outlook.com>
2022-11-21 01:43:56 +0200

    Setup shiftx v1

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Products</title>
+        <meta name="description" content="Products"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="container mx-auto">
+          <h1 className="h-1">Hello</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
