7b2637fdcb660dafec026d040483867004e24464
elxp101t3r <eliotx10101@outlook.com>
2022-12-06 20:29:35 +0200

    Searchbar

--- shiftx/pages/index.js
+++ shiftx/pages/index.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 import Product from "../components/Product";
 
 export default function Home() {
-    const [productsInfo, setProductsInfo] = useState([])
+    const [productsInfo, setProductsInfo] = useState([]);
+    const [phrase,setPhrase] = useState('');
     useEffect(() => {
       fetch('/api/products')
       .then(response => response.json())
@@ -11,19 +12,31 @@ export default function Home() {
     
     const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
 
+    let products;
+    if(phrase){
+      products = productsInfo.filter(p => p.name.toLowerCase().includes(phrase))
+    }else{
+      products = productsInfo;
+    }
+
   return (
       <div className="bg-purple-600 p-5">
+        <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search a product..." className="bg-purple-700 w-full py-2 px-4 rounded-xl"/>
         <div>
           {categoriesNames.map(categoryName => (
             <div key={categoryName}>
-              <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
-              <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
-              {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
-                <div key={productInfo._id} className="px-5 snap-start">
-                  <Product {...productInfo}/>
-                </div>
-              ))}
+              {products.find(p => p.category === categoryName) && (
+                <div>
+                  <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
+                  <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+                  {products.filter(p => p.category === categoryName).map(productInfo => (
+                    <div key={productInfo._id} className="px-5 snap-start">
+                      <Product {...productInfo}/>
+                    </div>
+                  ))}
               </div>
+                </div>
+              )}
               </div>
           ))}
           <div className="py-4">
