8a3f906e468ca51355accf8fea6c1bd84d0717bd
elxp101t3r <eliotx10101@outlook.com>
2022-12-06 19:32:30 +0200

    Make product component + retrieve data from db

--- shiftx/pages/index.js
+++ shiftx/pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import Product from "../components/Product";
 
 export default function Home() {
     const [productsInfo, setProductsInfo] = useState([])
@@ -9,26 +10,19 @@ export default function Home() {
     }, []);
     
     const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
-    console.log({categoriesNames});
 
   return (
       <div className="bg-purple-600 p-5">
         <div>
-          <h2 className="text-2xl">Audio</h2>
-          <div className="py-4">
-            <div className="w-64">
-              <div className="bg-white p-5 rounded-xl">
-                <img src="/products/razer.png" alt=""/>
-              </div>
-              <div className="mt-2">
-                <h3 className="font-bold text-lg">razer-thx</h3>
-              </div>
-              <p className="text-sm mt-1 leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-              <div className="flex mt-1">
-                  <div className="text-2xl font-bold grow">49.99$</div>
-                  <button className="bg-black text-white py-1 px-3 rounded-xl">+ to cart</button>
-              </div>
+          {categoriesNames.map(categoryName => (
+            <div key={categoryName}>
+              <h2 className="text-2xl capitalize">{categoryName}</h2>
+              {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
+                <Product {...productInfo}/>
+              ))}
             </div>
+          ))}
+          <div className="py-4">
           </div>
         </div>
       </div>
