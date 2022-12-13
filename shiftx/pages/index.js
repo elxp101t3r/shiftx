13ef294944a8a9a773f2c4a76baebc96ae6d8479
elxp101t3r <eliotx10101@outlook.com>
2022-12-13 11:06:15 +0200

    Fetching with getServerSideProps

--- shiftx/pages/index.js
+++ shiftx/pages/index.js
@@ -1,24 +1,16 @@
 import { useEffect, useState } from "react"
 import Product from "../components/Product";
+import { findAllProducts } from "./api/products";
 
-export default function Home() {
-    const [productsInfo, setProductsInfo] = useState([]);
+export default function Home({products}) {
     const [phrase,setPhrase] = useState('');
-    useEffect(() => {
-      fetch('/api/products')
-      .then(response => response.json())
-      .then(json => setProductsInfo(json));
-    }, []);
+ 
     
-    const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
+    const categoriesNames = [...new Set(products.map(p => p.category))];
 
-    let products;
     if(phrase){
-      products = productsInfo.filter(p => p.name.toLowerCase().includes(phrase))
-    }else{
-      products = productsInfo;
+      products = products.filter(p => p.name.toLowerCase().includes(phrase))
     }
-
   return (
       <div className="bg-purple-600 p-5">
         <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search a product..." className="bg-purple-700 w-full py-2 px-4 rounded-xl"/>
@@ -42,4 +34,14 @@ export default function Home() {
         </div>
       </div>
   )
+}
+
+export async function getServerSideProps(){
+  await initMongoose();
+  const products = await findAllProducts();
+  return{
+    props:{
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
 }
