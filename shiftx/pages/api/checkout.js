109ae219abd4ced11989ed199f3a8779364d15c3
elxp101t3r <eliotx10101@outlook.com>
2022-12-21 13:31:21 +0200

    Stripe is ready but needs a bug fix (quantity)

--- shiftx/pages/api/checkout.js
+++ shiftx/pages/api/checkout.js
@@ -1,4 +1,7 @@
 import { initMongoose } from "../../lib/mongoose";
+import Product from "../../models/Product";
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler(req,res){
     await initMongoose();
@@ -7,6 +10,32 @@ export default async function handler(req,res){
         res.body('Not post, but it wants post!').send();
         return;
     }
+
+    const productsIds = req.body.products.split(',');
+    const uniqIds = [...new Set(productsIds)];
+    const products = await Product.find({_id:{$in:uniqIds}}).exec();
+
+    let line_items = [];
+    for(let productId of uniqIds){
+        const quantity = productsIds.find(id => productId).length;
+        const product = products.find(p => p._id.toString() === productId); 
+        line_items.push({
+            quantity,
+            price_data:{
+                currency: 'EUR',
+                product_data: {name:product.name},
+                unit_amount: product.price * 100,
+            },
+        });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: line_items,
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.redirect(303, session.url);
 
     res.json(req.method);
 }
