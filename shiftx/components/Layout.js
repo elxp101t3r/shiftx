8debf6f2d5b717384f0f83e8a7a7a8a5075b987c
elxp101t3r <eliotx10101@outlook.com>
2022-12-22 00:18:59 +0200

    success message after payment

--- shiftx/components/Layout.js
+++ shiftx/components/Layout.js
@@ -1,8 +1,25 @@
+import { useContext, useEffect, useState } from "react";
 import Footer from "./Footer";
+import { ProductsContext } from "./ProductsContext";
+
+
 export default function Layout({children}){
+    const {setSelectedProducts} = useContext(ProductsContext);
+    const [success,setSuccess] = useState(false);
+    useEffect(() => {
+        if(window.location.href.includes('success')){
+            setSelectedProducts([]);
+            setSuccess(true);
+        }
+    }, []);
     return(
         <div>
             <div className="bg-purple-600 p-5">
+            {success && (
+                <div className="mb-5 bg-white text-purple-700 text-lg p-5 text-center">
+                    Thanks for choosing us!
+                </div>
+            )}
             {children} 
         </div>
         <Footer/>
